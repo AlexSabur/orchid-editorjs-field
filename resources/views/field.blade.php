899cd1c5745223ede6f3552f74e0b9ce5cd75312
AlexSabur <alexsabur@live.ru>
2021-10-27 18:26:22 +0300

    update resourses

--- resources/views/field.blade.php
+++ resources/views/field.blade.php
@@ -1,8 +1,12 @@
 @component($typeForm, get_defined_vars())
-<div data-controller="editorjs" data-editorjs-tools="{{ $tools }}">
+@if ($hidden)
+<input type="hidden" {{ $attributes }}>
+@else
+<div data-controller="editorjs" data-editorjs-tools="{{ $tools }}" data-editorjs-readonly="{{ var_export($readonly, true) }}" data-editorjs-placeholder="{{ $placeholder }}" data-editorjs-min-height="{{ $minHeight }}">
     {{-- @todo data-target="editorjs.holder" -> data-editorjs-target="holder" --}}
     <div data-target="editorjs.holder"></div>
     {{-- @todo data-target="editorjs.input" -> data-editorjs-target="input" --}}
-    <input data-target="editorjs.input" type="hidden" @attributes($attributes)>
+    <input data-target="editorjs.input" type="hidden" {{ $attributes }}>
 </div>
+@endif
 @endcomponent
