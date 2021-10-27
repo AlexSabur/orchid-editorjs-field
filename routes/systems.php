29d6292dca691ba7a279768f10f71d6d28bef64b
AlexSabur <alexsabur@live.ru>
2021-10-27 18:26:36 +0300

    wip

--- routes/systems.php
+++ routes/systems.php
@@ -2,12 +2,20 @@
 
 use AlexSabur\OrchidEditorJSField\Http\Controllers\ImageController;
 use AlexSabur\OrchidEditorJSField\Http\Controllers\LinkController;
+use Illuminate\Support\Facades\Route;
+use Orchid\Support\Facades\Dashboard;
 
-$this->router->post('image-by-file', [ImageController::class, 'byFile'])
-    ->name('systems.editorjs.image-by-file');
+Route::domain((string) config('platform.domain'))
+    ->prefix(Dashboard::prefix('/systems/editorjs'))
+    ->as('platform.systems.editorjs.')
+    ->middleware(config('platform.middleware.private'))
+    ->group(function () {
+        Route::post('/image-by-file', [ImageController::class, 'byFile'])
+            ->name('image-by-file');
 
-$this->router->post('image-by-url', [ImageController::class, 'byUrl'])
-    ->name('systems.editorjs.image-by-url');
+        Route::post('/image-by-url', [ImageController::class, 'byUrl'])
+            ->name('image-by-url');
 
-$this->router->get('link', LinkController::class)
-    ->name('systems.editorjs.link');
+        Route::get('/link', [LinkController::class, 'handle'])
+            ->name('link');
+    });
