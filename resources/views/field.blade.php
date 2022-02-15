18482db2aa7ceab9614b974ed3ea7a9ac44c688e
AlexSabur <alexsabur@live.ru>
2022-02-15 21:50:10 +0000

    update data attributes

--- resources/views/field.blade.php
+++ resources/views/field.blade.php
@@ -3,10 +3,8 @@
 <input type="hidden" {{ $attributes }}>
 @else
 <div data-controller="editorjs" data-editorjs-tools="{{ $tools }}" data-editorjs-readonly="{{ var_export($readonly, true) }}" data-editorjs-placeholder="{{ $placeholder }}" data-editorjs-min-height="{{ $minHeight }}">
-    {{-- @todo data-target="editorjs.holder" -> data-editorjs-target="holder" --}}
-    <div data-target="editorjs.holder"></div>
-    {{-- @todo data-target="editorjs.input" -> data-editorjs-target="input" --}}
-    <input data-target="editorjs.input" type="hidden" {{ $attributes }}>
+    <div data-editorjs-target="holder"></div>
+    <input data-editorjs-target="input" type="hidden" {{ $attributes }}>
 </div>
 @endif
 @endcomponent
